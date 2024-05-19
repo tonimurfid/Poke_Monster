15df3d3059ef4fa60bf917cbb8a726cbde5ee1e4
tonimurfid <fatonimurfids@gmail.com>
2024-05-19 09:43:12 +0700

    update 5

--- Item.java
+++ Item.java
@@ -1,13 +1,16 @@
 import java.io.Serializable;
 
 public class Item implements Serializable{
-    private String name;
-    private int effect;
+    private int effect = 20;
+    private static int maxUsed = 4;
+
+    public void getDamagePotion(Monster monster) {
+        if(maxUsed > 0) {
+            monster.setHP(monster.getHP() + effect);
+            maxUsed--;
+        }else{
+            System.out.println("Item ini sudah habis");
+        }
+    }
     
-    public Item(String name) {
-        this.name = name;
-    }
-    public String getName() {
-        return name;
-    }
 }
